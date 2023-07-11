3f0576faac08fe0281422f4e709d8ffeedb3b349
Andrey Troshkov <Andrey.Troshkov@enterra-inc.com>
2023-07-11 12:29:04 +0700

    добавил в лутлисты контейнеров v 1.0 готова

--- src/mod.ts
+++ src/mod.ts
@@ -1,44 +1,55 @@
 import {DependencyContainer} from "tsyringe";
 import {IPostAkiLoadMod} from "@spt-aki/models/external/IPostAkiLoadMod";
 import {IPostDBLoadMod} from "@spt-aki/models/external/IPostDBLoadMod";
-import {HashUtil} from "@spt-aki/utils/HashUtil";
 import {JsonUtil} from "@spt-aki/utils/JsonUtil";
 import {DatabaseServer} from "@spt-aki/servers/DatabaseServer";
 
 import * as config from "../config/config.json";
 
-class Mod implements IPostAkiLoadMod, IPostDBLoadMod {
+class Mod implements IPostAkiLoadMod, IPostDBLoadMod
+{
 	container: DependencyContainer;
 
-	public postAkiLoad(container: DependencyContainer): void {
+	public postAkiLoad(container: DependencyContainer): void
+{
 		this.container = container;
 	}
 
-	public postDBLoad(container: DependencyContainer): void {
+	public postDBLoad(container: DependencyContainer): void
+{
 		const jsonUtil = container.resolve<JsonUtil>("JsonUtil");
 		const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
 		const tables = databaseServer.getTables();
+		const loots = tables.loot.staticLoot;
 		const handbook = tables.templates.handbook;
 		const locales = Object.values(tables.locales.global) as Record<string, string>[];
 
 		const KEY_ID = "5448ba0b4bdc2d02308b456c";
-		const KEY_KYES_ID = "59fafd4b86f7745ca07e1232";
+		const KEY_KEYS_ID = "59fafd4b86f7745ca07e1232";
 
 		const itemId = "MA_MasterKey",
 			itemCategory = "5795f317245977243854e041",
 			itemFleaPrice = config.price,
 			itemName = "Отмычка",
 			itemShortName = "Отмычка",
-			itemDescription = "Одноразовая отмычка",
-			itemTraderPrice = config.price;
+			itemDescription = "Одноразовая отмычка, откроет любую дверь"
+
+		for (const loot in loots)
+		{
+			const lootItem = loots[loot];
+			lootItem.itemDistribution.push({
+				tpl: itemId,
+				relativeProbability: 30
+			});
+		}
+
 
 		const item = jsonUtil.clone(tables.templates.items[KEY_ID]);
-		const itemKeyCont = jsonUtil.clone(tables.templates.items[KEY_KYES_ID]);
+		const itemKeyCont = jsonUtil.clone(tables.templates.items[KEY_KEYS_ID]);
 
 		item._id = itemId;
 		item._props.Prefab.path = itemKeyCont._props.Prefab.path;
 		item._props.MaximumNumberOfUsage = 1;
-		item._props.Weight = 0.748;
 		item._props.InsuranceDisabled = true;
 		tables.templates.items[itemId] = item;
 
@@ -56,29 +67,6 @@ class Mod implements IPostAkiLoadMod, IPostDBLoadMod {
 				"Price": itemFleaPrice
 			}
 		);
-
-		const trader = tables.traders["5a7c2eca46aef81a7ca2145d"];
-
-		trader.assort.items.push({
-			"_id": itemId,
-			"_tpl": itemId,
-			"parentId": "hideout",
-			"slotId": "hideout",
-			"upd":
-				{
-					"UnlimitedCount": true,
-					"StackObjectsCount": 999999
-				}
-		});
-		trader.assort.barter_scheme[itemId] = [
-			[
-				{
-					"count": itemTraderPrice,
-					"_tpl": "5449016a4bdc2d6f028b456f" // roubles
-				}
-			]
-		];
-		trader.assort.loyal_level_items[itemId] = config.trader_loyalty_level;
 
 		this.allowIntoSecureContainers(itemId, tables.templates.items);
 	}
