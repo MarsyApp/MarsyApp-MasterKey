f4f38eafe1f17b75feb0d98f90c8293bceb50464
MarsyApp <MarsyApp>
2024-05-14 22:36:32 +0700

    small change

--- MypackageBuild.ts
+++ MypackageBuild.ts
@@ -1,32 +1,23 @@
 #!/usr/bin/env node
 
-// This is a simple script used to build a mod package. The script will copy necessary files to the build directory
-// and compress the build directory into a zip file that can be easily shared.
-
 const fs = require("fs-extra");
 const glob = require("glob");
 const zip = require('bestzip');
 const path = require("path");
 
-const pathToMods = "C:\\games\\EFT_3.5.8\\user\\mods"
+const pathToMods = "D:\\spt-381\\user\\mods"
 
-// Load the package.json file to get some information about the package so we can name things appropriately. This is
-// atypical, and you would never do this in a production environment, but this script is only used for development so
-// it's fine in this case. Some of these values are stored in environment variables, but those differ between node
-// versions; the 'author' value is not available after node v14.
 const { author, name:packageName, version } = require("./package.json");
 
-// Generate the name of the package, stripping out all non-alphanumeric characters in the 'author' and 'name'.
-const modName = `${author.replace(/[^a-z0-9]/gi, "")}-${packageName.replace(/[^a-z0-9]/gi, "")}-${version}`;
+const packageNameRes = packageName.replace(/[^a-z0-9]/gi, "");
+const packageNameRes2 = `MarsyApp-${packageNameRes}`;
+const modName = `${author.replace(/[^a-z0-9]/gi, "")}-${packageNameRes}-${version}`;
 console.log(`Generated package name: ${modName}`);
 
-// Delete the old build directory and compressed package file.
+fs.rmSync(`${__dirname}/${modName}`, { force: true, recursive: true });
 fs.rmSync(`${__dirname}/dist`, { force: true, recursive: true });
 console.log("Previous build files deleted.");
 
-// Generate a list of files that should not be copied over into the distribution directory. This is a blacklist to ensure
-// we always copy over additional files and directories that authors may have added to their project. This may need to be
-// expanded upon by the mod author to allow for node modules that are used within the mod; example commented out below.
 const ignoreList = [
     "node_modules/",
     // "node_modules/!(weighted|glob)", // Instead of excluding the entire node_modules directory, allow two node modules.
@@ -53,27 +44,31 @@ const ignoreList = [
     "test7.json",
     "allItemsName.json",
     "MypackageBuild.ts",
+    "clientMod",
+    "dist",
     modName
 ];
 const exclude = glob.sync(`{${ignoreList.join(",")}}`, { realpath: true, dot: true });
 
-// For some reason these basic-bitch functions won't allow us to copy a directory into itself, so we have to resort to
-// using a temporary directory, like an idiot. Excuse the normalize spam; some modules cross-platform, some don't...
 fs.copySync(__dirname, path.normalize(`${__dirname}/../~${modName}`), {filter:(filePath) =>
-{
-    return !exclude.includes(filePath);
-}});
+    {
+        return !exclude.includes(filePath);
+    }});
 fs.moveSync(path.normalize(`${__dirname}/../~${modName}`), path.normalize(`${__dirname}/${modName}`), { overwrite: true });
-fs.copySync(path.normalize(`${__dirname}/${modName}`), path.normalize(`${pathToMods}/MarsyApp-MasterKey`));
+fs.copySync(path.normalize(`${__dirname}/${modName}`), path.normalize(`${pathToMods}/${packageNameRes2}`));
 console.log("Build files copied.");
 
-// Compress the files for easy distribution. The compressed file is saved into the dist directory. When uncompressed we
-// need to be sure that it includes a directory that the user can easily copy into their game mods directory.
-/*
+fs.copySync(path.normalize(`${__dirname}/${modName}`), path.normalize(`${__dirname}/dist/user/mods/${packageNameRes2}`));
+fs.copySync(path.normalize(`${__dirname}/clientMod/bin/Release/net472/${packageNameRes2}.dll`), path.normalize(`${__dirname}/dist/BepInEx/plugins/${packageNameRes2}.dll`));
+
+console.log("dist files copied.");
+
+const dirsArray = fs.readdirSync(`${__dirname}/dist`);
+console.log(dirsArray);
 zip({
-    source: modName,
-    destination: `dist/${modName}.zip`,
-    cwd: __dirname
+    source: dirsArray,
+    destination: `${modName}.zip`,
+    cwd: `${__dirname}/dist`,
 }).catch(function(err)
 {
     console.error("A bestzip error has occurred: ", err.stack);
@@ -81,7 +76,6 @@ zip({
 {
     console.log(`Compressed mod package to: /dist/${modName}.zip`);
 
-    // Now that we're done with the compression we can delete the temporary build directory.
     fs.rmSync(`${__dirname}/${modName}`, { force: true, recursive: true });
     console.log("Build successful! your zip file has been created and is ready to be uploaded to hub.sp-tarkov.com/files/");
-});*/
+});
